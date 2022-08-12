d7a4ff001cbfa4ae6dba09994c7c86b0a8fa61c9
Diego Bryan <thediegobryan@gmail.com>
2022-08-12 18:07:03 -0400

    installed and configured djoser

--- core/settings.py
+++ core/settings.py
@@ -47,6 +47,8 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'rest_framework',
+    'rest_framework.authtoken',
+    'djoser',
     'django_extensions',
     'api',
 ]
