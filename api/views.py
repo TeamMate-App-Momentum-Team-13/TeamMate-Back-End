1f2dff8e494f1f35300c687848f2d262280f36d8
Diego Bryan <thediegobryan@gmail.com>
2022-08-13 15:04:49 -0400

    added game session view

--- api/views.py
+++ api/views.py
@@ -1,3 +1,19 @@
 from django.shortcuts import render
+from rest_framework.generics import CreateAPIView, DestroyAPIView, ListAPIView, ListCreateAPIView, RetrieveUpdateDestroyAPIView
+from rest_framework.response import Response
+
+from .models import User, GameSession, Court, CourtAddress, UserAddress, Guest, Profile, AddressModelMixin
 
 # Create your views here.
+
+@api_view(['GET'])
+def welcome(request):
+    return Response({
+        'team': 'Team Swan Lake',
+        'description': 'Welcome to our app 👋'
+    })
+
+class NewGameSession(ListCreateAPIView):
+    queryset = GameSession.objects.all()
+    serializer_class = GameSessionSerializer
+    
