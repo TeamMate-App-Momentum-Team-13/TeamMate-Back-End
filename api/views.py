54f611e1dd89379ec2431240deea0901c4795c86
Yuriy Golota <ypgolota@pm.me>
2022-08-13 22:42:29 -0400

    refactored imports for clarity, overwrote perform_create method for POST GameSession view

--- api/views.py
+++ api/views.py
@@ -1,11 +1,17 @@
 from django.shortcuts import render
-from rest_framework.generics import CreateAPIView, DestroyAPIView, ListAPIView, ListCreateAPIView, RetrieveUpdateDestroyAPIView
+from rest_framework.generics import (
+    CreateAPIView, 
+    DestroyAPIView, 
+    ListAPIView, 
+    ListCreateAPIView, 
+    RetrieveUpdateDestroyAPIView, 
+)
 from rest_framework.response import Response
 
 from .models import User, GameSession, Court, CourtAddress, UserAddress, Guest, Profile, AddressModelMixin
 
 from .serializers import GameSessionSerializer
-# Create your views here.
+
 
 def welcome(request):
     return Response({
@@ -13,7 +19,10 @@ def welcome(request):
         'description': 'Welcome to our app 👋'
     })
 
-class NewGameSession(ListCreateAPIView):
+
+class ListCreateGameSession(ListCreateAPIView):
     queryset = GameSession.objects.all()
     serializer_class = GameSessionSerializer
-    
+
+    def perform_create(self, serializer):
+        serializer.save(host=self.request.user)
