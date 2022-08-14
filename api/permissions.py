bee62cb12be7fb3dcb9e760ecc9c775b4f3e170d
Diego Bryan <thediegobryan@gmail.com>
2022-08-14 09:57:59 -0400

    added permissions.py and retrieve update destroy game session view

---
+++ api/permissions.py
@@ -0,0 +1,15 @@
+from rest_framework import permissions
+
+
+# allows only owner of object to access object
+class IsOwner(permissions.BasePermission):
+    def has_object_permission(self, request, view, obj):
+        return obj.user == request.user
+
+
+# allows only owner of object to edit object
+class IsOwnerOrReadOnly(permissions.BasePermission):
+    def has_object_permission(self, request, view, obj):
+        if request.method in permissions.SAFE_METHODS:
+            return True
+        return obj.user_id == request.user
