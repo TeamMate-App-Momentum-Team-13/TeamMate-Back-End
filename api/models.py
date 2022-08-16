dce092513c253645d5ac08a1f169817a6c912904
Yuriy Golota <ypgolota@pm.me>
2022-08-16 13:40:51 -0400

    add profile_pic back in, to avoid migration errors at this stage

--- api/models.py
+++ api/models.py
@@ -117,4 +117,5 @@ class Profile(BaseModel):
     ]
 
     user = models.ForeignKey(User, on_delete=models.CASCADE, related_name='profile')
+    profile_pic = models.TextField(blank=True, null=True)
     ntrp_rating = models.CharField(max_length=10, choices=RATE_CHOICES, default=TWOFIVE)
