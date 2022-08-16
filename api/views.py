6869481b2f7427bd402026fd6ed74344e0fe9996
Diego Bryan <thediegobryan@gmail.com>
2022-08-16 08:54:57 -0400

    spent time working with api view and custom methods, need to switch to function based views in order to apply custom permissions

--- api/views.py
+++ api/views.py
@@ -1,6 +1,8 @@
+from functools import partial
 from django.shortcuts import render, get_object_or_404
-from rest_framework import permissions
+from rest_framework import permissions, status, views
 from rest_framework.views import APIView
+from rest_framework.decorators import permission_classes, api_view
 from .permissions import IsOwnerOrReadOnly, IsOwner
 from rest_framework.response import Response
 from .serializers import GameSessionSerializer, GuestSerializer
@@ -23,8 +25,6 @@ from .models import (
     Profile, 
     AddressModelMixin
 )
-
-
 
 def welcome(request):
     return Response({
@@ -57,16 +57,20 @@ class ListCreateGuest(ListCreateAPIView):
         game_session_instance = get_object_or_404(GameSession, pk=self.kwargs.get('pk'))
         serializer.save(user=self.request.user, game_session=game_session_instance)
         
-# class GameSessionGuest(RetrieveUpdateDestroyAPIView):
-#     queryset = Guest.objects.all()
-#     serializer_class = GuestSerializer
-#     lookup_url_kwarg = 'guest_pk'
-
 class GameSessionGuest(APIView):
+    permission_classes = (permissions.IsAuthenticatedOrReadOnly)
     lookup_url_kwarg = 'guest_pk'
 
     def get(self, request, **kwargs):
         guest_detail = get_object_or_404(Guest, pk=self.kwargs.get('guest_pk'))
         serializer = GuestSerializer(guest_detail, many=False)
         return Response(serializer.data)
+
+    def patch (self, request, **kwargs):
+        guest_detail = get_object_or_404(Guest, pk=self.kwargs.get('guest_pk'))
+        serializer = GuestSerializer(guest_detail, data=request.data, partial=True)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
