602408418d77e2958ba719b37fd9cfb2da3eeb7f
Yuriy Golota <ypgolota@pm.me>
2022-08-18 17:02:30 -0400

    add URL path for ListCreateCourtAddress

--- core/urls.py
+++ core/urls.py
@@ -18,4 +18,5 @@ urlpatterns = [
     path('session/', api_views.ListCreateGameSession.as_view(), name='game-session-list'),
     path('session/<int:pk>', api_views.RetrieveUpdateDestroyGameSession.as_view(), name='game-session-detail'),
     path('court/', api_views.ListCreateCourt.as_view(), name='court'),
+    path('court/<int:pk>/address/', api_views.ListCreateCourtAddress.as_view(), name='court-address'),
 ]
