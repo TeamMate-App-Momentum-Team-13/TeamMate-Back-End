9e2360b8aba3bd9d2607d12f36961957f68426fc
Diego Bryan <thediegobryan@gmail.com>
2022-08-25 08:43:27 -0400

    added Score to rank converter function

--- api/algorithm.py
+++ api/algorithm.py
@@ -1,4 +1,9 @@
-def RankCalculation(user_score, oponent_score, win_loss):
+# def RankCalculation(user_score, oponent_score, win_loss)
+def RankCalculation():
+    user_score = int(input("what is your score"))
+    oponent_score = int(input("what is your oponents score"))
+    win_loss = input("win or loss")
+
     if win_loss == "win":
         if abs(user_score - oponent_score) <= 25:
             user_score += 8
@@ -26,5 +31,47 @@ def RankCalculation(user_score, oponent_score, win_loss):
         user_score = 450
     if user_score < 0:
         user_score = 0
-
+    
+    print(user_score)
     return user_score
+
+def ScoreToRankConverter(user_score):
+    
+    if user_score <= 25:
+        rank = "TwoFiveBronze"
+    elif user_score <= 50:
+        rank = "TwoFiveSilver"
+    elif user_score <= 75:
+        rank = "TwoFiveGold"
+    elif user_score <= 100:
+        rank = "ThreeBronze"
+    elif user_score <= 125:
+        rank = "ThreeSilver"
+    elif user_score <= 150:
+        rank = "ThreeGold"
+    elif user_score <= 175:
+        rank = "ThreeFiveBronze"
+    elif user_score <= 200:
+        rank = "ThreeFiveSilver"
+    elif user_score <= 225:
+        rank = "ThreeFiveGold"
+    elif user_score <= 250:
+        rank = "FourBronze"
+    elif user_score <= 275:
+        rank = "FourSilver"
+    elif user_score <= 300:
+        rank = "FourGold"
+    elif user_score <= 325:
+        rank = "FourFiveBronze"
+    elif user_score <= 350:
+        rank = "FourFiveSilver"
+    elif user_score <= 375:
+        rank = "FourFiveGold"
+    elif user_score <= 400:
+        rank = "FiveBronze"
+    elif user_score <= 425:
+        rank = "FiveSilver"
+    elif user_score <= 450:
+        rank = "FiveGold"
+    
+    return rank
