ad5e273417749ff4c1c64401064cf56f5fd9406b
Diego Bryan <thediegobryan@gmail.com>
2022-08-27 09:58:14 -0400

    grammer changes

--- api/models.py
+++ api/models.py
@@ -23,14 +23,14 @@ def notification_created_or_updated_guest_handler(sender, instance, created, *ar
             NotificationGameSession.objects.create(
                 sender=instance.user,
                 reciever=instance.game_session.host,
-                message=(f"Good news, {instance.user.first_name} would like you join your game on {clean_date} at {clean_time}. Please go to MyGames to respond."),
+                message=(f"Good news, {instance.user.first_name} would like to join your game on {clean_date} at {clean_time}. Please go to My Games to respond."),
                 game_session = instance.game_session,
             )
     else: 
         print("Guest has been updated")
         update_game_session_confirmed_field(instance.game_session.pk)
         if instance.status == "Accepted":
-            response = f"Yay! {instance.game_session.host.first_name} has confirmed your game on {clean_date} at {clean_time}. You can see all of your confirmed games on the MyGames page."
+            response = f"Yay! {instance.game_session.host.first_name} has confirmed your game on {clean_date} at {clean_time}. You can see all of your confirmed games on the My Games page."
         elif instance.status == "Rejected":
             response = f"Darn, {instance.game_session.host.first_name} isn't available to play on {clean_date} anymore, but you can sign up for a different game on the Open Games page."
         else:
